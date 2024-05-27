1bb20c9dedc0fc5597279b3bdba3b1648d7172ce
CryingPerson <opnice12@naver.com>
2024-05-27 20:33:06 +0900

    Initial commit

---
+++ src/main/java/com/example/bufsroom/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.bufsroom.service;
+
+import com.example.bufsroom.DataNotFoundException;
+import com.example.bufsroom.repository.UserRepository;
+import com.example.bufsroom.user.SiteUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public SiteUser create(String username, String password) {
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+	}
+
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+		if (siteUser.isPresent()) {
+			return siteUser.get();
+		} else {
+			throw new DataNotFoundException("siteuser not found");
+		}
+	}
+}
---
+++ src/main/java/com/example/bufsroom/user/SiteUser.java
@@ -0,0 +1,21 @@
+package com.example.bufsroom.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String username;
+
+	private String password;
+
+}
